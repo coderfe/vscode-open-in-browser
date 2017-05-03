569d38faaef6652f1e8e5463070b7eb39b942c9f
coderfee <coderfee@outlook.com>
2017-05-03 22:45:34 +0800

    add detect default app script

--- extension.js
+++ extension.js
@@ -3,17 +3,22 @@ const opn = require('opn')
 const platform = require('os').platform()
 const defaultBrowser = require('x-default-browser')
 
+let winChrome = 'chrome'
+let osxChrome = 'google chrome'
+let linuxChrome = 'google-chrome'
+let linuxChromium = 'chromium-browser'
+
 function activate (context) {
   let editor = vscode.window.activeTextEditor
   let doc = editor.document
   const disposable = vscode.commands.registerCommand('extension.openWith', function () {
     let quickPick
     if (platform === 'win32') {  // Windows
-      quickPick = ['chrome', 'firefox', 'iexplore']
+      quickPick = [winChrome, 'firefox', 'iexplore']
     } else if (platform === 'darwin') {  // OS X
-      quickPick = ['google chrome', 'firefox']
+      quickPick = [osxChrome, 'firefox']
     } else {  // Linux
-      quickPick = ['chromium-browser', 'google-chrome', 'firefox']
+      quickPick = ['chromium-browser', linuxChrome, 'firefox']
     }
     vscode.window.showQuickPick(quickPick, { placeHolder: 'Which browser?' }).then((val) => {
       if (val) {
@@ -28,12 +33,25 @@ function activate (context) {
   const disposableDefault = vscode.commands.registerCommand('extension.openWithDefault', () => {
 
     defaultBrowser(function (error, result){
-      if(!error)
-        opn(doc.fileName, { app: result.commonName })
+      if(!error) {
+        let appName = null;
+        if (result.commonName === 'chrome') {
+          if (platform === 'win32') {
+            appName = winChrome
+          } else if (platform === 'darwin') {
+            appName = osxChrome
+          } else {
+            appName = linuxChrome
+          }
+        } else if (result.commonName === 'chromium') {
+          appName = linuxChromium
+        }
+        opn(doc.fileName, { app: appName })
+      }
       else
         opn(doc.fileName)
-    })  
-    
+    })
+
   })
   context.subscriptions.push(disposableDefault)
 }
